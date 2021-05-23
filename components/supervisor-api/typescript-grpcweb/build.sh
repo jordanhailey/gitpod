fed17c3121af9c53ee10f7caf6c04353febc803f
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2021-05-23 16:20:13 -0400

    Run shellcheck against scripts

--- components/supervisor-api/typescript-grpcweb/build.sh
+++ components/supervisor-api/typescript-grpcweb/build.sh
@@ -3,20 +3,21 @@
 # Licensed under the GNU Affero General Public License (AGPL).
 # See License-AGPL.txt in the project root for license information.
 
-DIR=$(cd $(dirname "${BASH_SOURCE}") && pwd -P)
+set -e
+
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
 
 THIRD_PARTY_INCLUDES=${PROTOLOC:-$DIR/..}
-if [ ! -d $THIRD_PARTY_INCLUDES/third_party/google/api ]; then
+if [ ! -d "$THIRD_PARTY_INCLUDES"/third_party/google/api ]; then
     echo "missing $THIRD_PARTY_INCLUDES/third_party/google/api"
-    exit -1
+    exit 1
 fi
 
-
 mkdir -p lib
-export PROTO_INCLUDE="-I$THIRD_PARTY_INCLUDES/third_party -I /usr/lib/protoc/include"
 
-protoc $PROTO_INCLUDE \
+protoc \
+    -I"$THIRD_PARTY_INCLUDES"/third_party -I/usr/lib/protoc/include \
     --plugin="protoc-gen-ts=$DIR/node_modules/.bin/protoc-gen-ts" \
     --js_out="import_style=commonjs,binary:lib" \
     --ts_out="service=grpc-web:lib" \
-    -I${PROTOLOC:-..} ${PROTOLOC:-..}/*.proto
+    -I"${PROTOLOC:-..}" "${PROTOLOC:-..}"/*.proto
